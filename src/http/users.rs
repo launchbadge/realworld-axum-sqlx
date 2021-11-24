eda31d65887f5565a8fbc93d867161c9ca1c1ba1
Austin Bonander <austin@launchbadge.com>
2021-11-24 13:51:43 -0800

    feat: implement remaining users routes

--- src/http/users.rs
+++ src/http/users.rs
@@ -3,14 +3,19 @@ use anyhow::Context;
 use argon2::password_hash::SaltString;
 use argon2::{Argon2, PasswordHash};
 use axum::extract::Extension;
-use axum::routing::post;
+use axum::routing::{get, post};
 use axum::{Json, Router};
 
 use crate::http::error::{Error, ResultExt};
 use crate::http::extractor::AuthUser;
 
 pub fn router() -> Router {
-    Router::new().route("/api/users", post(create_user))
+    // By having each module responsible for setting up its own routing,
+    // it makes the root module a lot cleaner.
+    Router::new()
+        .route("/api/users", post(create_user))
+        .route("/api/users/login", post(login_user))
+        .route("/api/user", get(get_current_user).put(update_user))
 }
 
 /// A wrapper type for all requests/responses from these routes.
@@ -26,6 +31,22 @@ struct NewUser {
     password: String,
 }
 
+#[derive(serde::Deserialize)]
+struct LoginUser {
+    username: String,
+    password: String,
+}
+
+#[derive(serde::Deserialize, Default, PartialEq, Eq)]
+#[serde(default)] // fill in any missing fields with `..UpdateUser::default()`
+struct UpdateUser {
+    email: Option<String>,
+    username: Option<String>,
+    password: Option<String>,
+    bio: Option<String>,
+    image: Option<String>,
+}
+
 #[derive(serde::Serialize, serde::Deserialize)]
 struct User {
     email: String,
@@ -35,6 +56,7 @@ struct User {
     image: Option<String>,
 }
 
+// https://gothinkster.github.io/realworld/docs/specs/backend-specs/endpoints#registration
 async fn create_user(
     ctx: Extension<ApiContext>,
     Json(req): Json<UserBody<NewUser>>,
@@ -68,7 +90,132 @@ async fn create_user(
     }))
 }
 
+// https://gothinkster.github.io/realworld/docs/specs/backend-specs/endpoints#authentication
+async fn login_user(
+    ctx: Extension<ApiContext>,
+    Json(req): Json<UserBody<LoginUser>>,
+) -> Result<Json<UserBody<User>>> {
+    let user = sqlx::query!(
+        r#"
+            select user_id, email, username, bio, image, password_hash 
+            from "user" where username = $1
+        "#,
+        req.user.username,
+    )
+    .fetch_optional(&ctx.db)
+    .await?
+    .ok_or(Error::unprocessable_entity([(
+        "username",
+        "does not exist",
+    )]))?;
+
+    verify_password(req.user.password, user.password_hash).await?;
+
+    Ok(Json(UserBody {
+        user: User {
+            email: user.email,
+            token: AuthUser {
+                user_id: user.user_id,
+            }
+            .to_jwt(&ctx),
+            username: user.username,
+            bio: user.bio,
+            image: user.image,
+        },
+    }))
+}
+
+// https://gothinkster.github.io/realworld/docs/specs/backend-specs/endpoints#get-current-user
+async fn get_current_user(
+    auth_user: AuthUser,
+    ctx: Extension<ApiContext>,
+) -> Result<Json<UserBody<User>>> {
+    let user = sqlx::query!(
+        r#"select email, username, bio, image from "user" where user_id = $1"#,
+        auth_user.user_id
+    )
+    .fetch_one(&ctx.db)
+    .await?;
+
+    Ok(Json(UserBody {
+        user: User {
+            email: user.email,
+            // The spec doesn't state whether we're supposed to return the same token we were passed,
+            // or generate a new one. Generating a new one is easier the way the code is structured.
+            //
+            // This has the side-effect of automatically refreshing the session if the frontend
+            // updates its token based on this response.
+            token: auth_user.to_jwt(&ctx),
+            username: user.username,
+            bio: user.bio,
+            image: user.image,
+        },
+    }))
+}
+
+// https://gothinkster.github.io/realworld/docs/specs/backend-specs/endpoints#update-user
+// Semantically, because this route allows a partial update it should be `PATCH`, not `PUT`.
+// However, we have a spec to follow so `PUT` it is.
+async fn update_user(
+    auth_user: AuthUser,
+    ctx: Extension<ApiContext>,
+    Json(req): Json<UserBody<UpdateUser>>,
+) -> Result<Json<UserBody<User>>> {
+    if req.user == UpdateUser::default() {
+        // If there's no fields to update, these two routes are effectively identical.
+        return get_current_user(auth_user, ctx).await;
+    }
+
+    // WTB `Option::map_async()`
+    let password_hash = if let Some(password) = req.user.password {
+        Some(hash_password(password).await?)
+    } else {
+        None
+    };
+
+    let user = sqlx::query!(
+        // This is how we do optional updates of fields without needing a separate query for each.
+        // language=PostgreSQL
+        r#"
+            update "user"
+            set email = coalesce($1, "user".email),
+                username = coalesce($2, "user".username),
+                password_hash = coalesce($3, "user".password_hash),
+                bio = coalesce($4, "user".bio),
+                image = coalesce($5, "user".image)
+            where user_id = $6
+            returning email, username, bio, image
+        "#,
+        req.user.email,
+        req.user.username,
+        password_hash,
+        req.user.bio,
+        req.user.image,
+        auth_user.user_id
+    )
+    .fetch_one(&ctx.db)
+    .await
+    .on_constraint("user_username_key", |_| {
+        Error::unprocessable_entity([("username", "username taken")])
+    })
+    .on_constraint("user_email_key", |_| {
+        Error::unprocessable_entity([("email", "email taken")])
+    })?;
+
+    Ok(Json(UserBody {
+        user: User {
+            email: user.email,
+            token: auth_user.to_jwt(&ctx),
+            username: user.username,
+            bio: user.bio,
+            image: user.image,
+        },
+    }))
+}
+
 async fn hash_password(password: String) -> Result<String> {
+    // Argon2 hashing is designed to be computationally intensive,
+    // so we need to do this on a blocking thread.
     Ok(tokio::task::spawn_blocking(move || -> Result<String> {
         let salt = SaltString::generate(rand::thread_rng());
         Ok(
@@ -79,4 +226,19 @@ async fn hash_password(password: String) -> Result<String> {
     })
     .await
     .context("panic in generating password hash")??)
+}
+
+async fn verify_password(password: String, password_hash: String) -> Result<()> {
+    Ok(tokio::task::spawn_blocking(move || -> Result<()> {
+        let hash = PasswordHash::new(&password_hash)
+            .map_err(|e| anyhow::anyhow!("invalid password hash: {}", e))?;
+
+        hash.verify_password(&[&Argon2::default()], password)
+            .map_err(|e| match e {
+                argon2::password_hash::Error::Password => Error::Unauthorized,
+                _ => anyhow::anyhow!("failed to verify password hash: {}", e).into(),
+            })
+    })
+    .await
+    .context("panic in verifying password hash")??)
 }
