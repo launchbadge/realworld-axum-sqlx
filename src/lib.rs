f10b1dfc146d3b8f7c753fa6327d67c1f7e12c38
Austin Bonander <austin@launchbadge.com>
2022-01-25 20:49:06 -0800

    docs(lib.rs): document `http` module

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,7 @@
 /// [`clap`]: https://github.com/clap-rs/clap/
 pub mod config;
 
+/// Contains the setup code for the API build with Axum.
+///
+/// The Realworld API routes exist in child modules of this.
 pub mod http;
